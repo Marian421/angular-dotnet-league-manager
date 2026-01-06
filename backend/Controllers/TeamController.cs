30aa50c667809ced47d8440e27d5f0f0c3b8b904
Herciu Marian <herciumarian8@gmail.com>
2026-01-06 12:16:06 +0200

    Method for creating team in TeamController

--- backend/Controllers/TeamController.cs
+++ backend/Controllers/TeamController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 using backend.Data;
+using backend.DTOs.Teams;
 
 namespace backend.Controllers
 {
@@ -40,6 +41,20 @@ namespace backend.Controllers
             }
 
             return Ok(team);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostTeam([FromBody] TeamCreateDto dto)
+        {
+            try
+            {
+                var createdTeam = await _teamService.CreateTeamAsync(dto);
+                return CreatedAtAction(nameof(GetTeam), new { id = createdTeam.Id }, createdTeam);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
