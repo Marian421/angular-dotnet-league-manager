5c0e9473f2d02254ca5e777890408d0eb924dc6d
Herciu Marian <herciumarian8@gmail.com>
2026-01-10 20:08:26 +0200

    feat(client): add TeamCreate method to team services

--- client/league-manager/src/app/core/services/teams/teams.ts
+++ client/league-manager/src/app/core/services/teams/teams.ts
@@ -2,20 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Team } from '../../../shared/models/team.model';
+import { environment } from '../../../../environments/environment';
+import { TeamCreate } from '../../../shared/models/team-create.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TeamsService {
-  private apiUrl = "url";
+  private readonly apiUrl = environment.apiBaseUrl;
+  private readonly teamsUrl = `${this.apiUrl}/teams`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTeams() {
-    return this.http.get<Team[]>(this.apiUrl);
+    return this.http.get<Team[]>(this.teamsUrl);
   }
 
   getTeamById(id: string): Observable<Team> {
-    return this.http.get<Team>(`${this.apiUrl}/${id}`);
+    return this.http.get<Team>(`${this.teamsUrl}/${id}`);
+  }
+
+  createTeam(team: TeamCreate) {
+    return this.http.post<Team>(this.teamsUrl, team);
   }
 }
